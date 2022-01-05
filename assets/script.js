fae17558a95c2d8fe378a614b1cfcd7deb9c16a0
Javier Delcid <javierldelcid@gmail.com>
2022-01-04 18:25:40 -0800

    add some script

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var date = new Date()
+console.log(date)
+var key = '9cfe7036b90b3a13af1a88f6bf534b32'
+var searchInput = document.getElementById('search')
+var searchBtn = document.getElementById('srchBtn')
+var srchHistory = document.getElementById('history')
+var forecastEl = document.getElementById('forecast')
+console.log(forecastEl)
+console.log(searchBtn)
+var currentWeather = document.querySelector('.current-weather-display')
+console.log(currentWeather)
+var weatherIconUrl = 'https://openweathermap.org/img/w/10d.png'
